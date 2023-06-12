8add127fba72b4c569b98f82a00ad1e0ce5e4dad
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2023-06-13 01:30:22 +0300

    update card

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,16 @@
-import './ExpenseItem.css';
-import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
+import Card from './Card.js'
+
 function ExpenseItem (props) {
   return (
-    <div className="expense-item">
-      <ExpenseDate date={props.date}></ExpenseDate>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-    </div>
-  );
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date}> </ExpenseDate>{' '}
+      <div className='expense-item__description'>
+        <h2> {props.title} </h2>{' '}
+        <div className='expense-item__price'> $ {props.amount} </div>{' '}
+      </div>{' '}
+    </Card>
+  )
 }
-export default ExpenseItem;
+export default ExpenseItem
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,10 +1,11 @@
 import ExpenseItem from './ExpenseItem'
+import Card from './Card.js'
 
 import './Expenses.css'
 
 function Expenses (props) {
   return (
-    <div className='expenses'>
+    <Card className='expenses'>
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
@@ -25,7 +26,7 @@ function Expenses (props) {
         amount={props.expenses[3].amount}
         date={props.expenses[3].date}
       />{' '}
-    </div>
+    </Card>
   )
 }
 
