3bd527ab4b70e1dd9c8ae707ace50d60e62409a2
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2023-06-13 01:08:49 +0300

    update css

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,18 +1,18 @@
-import './ExpenseDate.css';
+import './ExpenseDate.css'
 
 function ExpenseDate (props) {
-  const month = props.date.toLocaleString ('en-US', {month: 'long'});
-  const day = props.date.toLocaleString ('en-US', {day: '2-digit'});
-  const year = props.date.getFullYear ();
+  const month = props.date.toLocaleString('en-US', { month: 'long' })
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' })
+  const year = props.date.getFullYear()
 
   return (
-    <div className="expense-item">
+    <div className='expense-date'>
       <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+        <div className='expense-date__month'> {month} </div>{' '}
+        <div className='expense-date__year'> {year} </div>{' '}
+        <div className='expense-date__day'> {day} </div>{' '}
+      </div>{' '}
     </div>
-  );
+  )
 }
-export default ExpenseDate;
+export default ExpenseDate
