b297a63ec1f6dffd952fabd8ce4614bd2a3385cb
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2023-06-13 01:33:01 +0300

    fix bug in expense date

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -7,12 +7,11 @@ function ExpenseDate (props) {
 
   return (
     <div className='expense-date'>
-      <div>
-        <div className='expense-date__month'> {month} </div>{' '}
-        <div className='expense-date__year'> {year} </div>{' '}
-        <div className='expense-date__day'> {day} </div>{' '}
-      </div>{' '}
+      <div className='expense-date__month'> {month} </div>{' '}
+      <div className='expense-date__year'> {year} </div>{' '}
+      <div className='expense-date__day'> {day} </div>{' '}
     </div>
   )
 }
+
 export default ExpenseDate
