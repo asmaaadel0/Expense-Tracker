5bf15f646874395cc27ed69bea21d79ff859a243
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2023-06-13 15:25:38 +0300

    add state for expense item

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 import './ExpenseItem.css'
 
 const ExpenseItem = props => {
-  return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />{' '}
-      <div className='expense-item__description'>
-        <h2> {props.title} </h2>{' '}
-        <div className='expense-item__price'> $ {props.amount} </div>{' '}
-      </div>{' '}
-    </Card>
-  )
+    const [title, setTitle] = useState(props.title)
+
+    // let title = props.title
+    const clickHandler = () => {
+        setTitle('updated')
+    }
+    return ( <
+        Card className = 'expense-item' >
+        <
+        ExpenseDate date = { props.date }
+        />{' '} <
+        div className = 'expense-item__description' >
+        <
+        h2 > { title } < /h2>{' '} <
+        div className = 'expense-item__price' > $ { props.amount } < /div>{' '} <
+        /div>{' '} <
+        button onClick = { clickHandler } > Change Title < /button>{' '} <
+        /Card>
+    )
 }
 
 export default ExpenseItem
