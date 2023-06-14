9ae8f5e50522077d5954ab9de3a7261746d7765a
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2023-06-14 23:42:20 +0300

    update form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,59 +3,62 @@ import React, { useState } from 'react'
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
-  // const [enteredTitle, setEnteredTitle] = useState('')
-  // const [enteredDate, setEnteredDate] = useState('')
-  // const [enteredAmount, setEnteredAmount] = useState('')
+  const [enteredTitle, setEnteredTitle] = useState('')
+  const [enteredDate, setEnteredDate] = useState('')
+  const [enteredAmount, setEnteredAmount] = useState('')
 
-  const [userInput, setUserInput] = useState({
-    enteredTitle: '',
-    enteredDate: '',
-    enteredAmount: ''
-  })
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredDate: '',
+  //   enteredAmount: ''
+  // })
 
-  // const titleChangeHandler = event => {
-  //   // setUserInput({ ...userInput, enteredTitle: event.target.value })
-  //   setUserInput(prevState => {
-  //     return { ...prevState, enteredTitle: event.target.value }
-  //   })
-  // }
-  // const dateChangeHandler = event => {
-  //   // setUserInput({ ...userInput, enteredDate: event.target.value })
-  //   setUserInput(prevState => {
-  //     return { ...prevState, enteredDate: event.target.value }
-  //   })
-  // }
-  // const amountChangeHandler = event => {
-  //   // setUserInput({ ...userInput, enteredAmount: event.target.value })
-  //   setUserInput(prevState => {
-  //     return { ...prevState, enteredAmount: event.target.value }
-  //   })
+  const titleChangeHandler = event => {
+    setEnteredTitle(event.target.value)
+    // setUserInput(prevState => {
+    //   return { ...prevState, enteredTitle: event.target.value }
+    // })
+  }
+  const dateChangeHandler = event => {
+    setEnteredDate(event.target.value)
+  }
+  const amountChangeHandler = event => {
+    setEnteredAmount(event.target.value)
+  }
+
+  // const inputChangeHandler = (indetifier, value) => {
+  //   if (indetifier === 'title') {
+  //     setEnteredTitle(value)
+  //   } else if (indetifier === 'date') {
+  //     setEnteredDate(value)
+  //   } else {
+  //     setEnteredAmount(value)
+  //   }
   // }
 
-  const inputChangeHandler = (indetifier, value) => {
-    if (indetifier === 'title') {
-      setUserInput(prevState => {
-        return { ...prevState, enteredTitle: value }
-      })
-    } else if (indetifier === 'date') {
-      setUserInput(prevState => {
-        return { ...prevState, enteredDate: value }
-      })
-    } else {
-      setUserInput(prevState => {
-        return { ...prevState, enteredAmount: value }
-      })
+  const submitHandler = event => {
+    event.preventDefault()
+
+    const userInput = {
+      title: enteredTitle,
+      date: enteredDate,
+      amount: enteredAmount
     }
+    console.log(userInput)
+    setEnteredTitle('')
+    setEnteredDate('')
+    setEnteredAmount('')
   }
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label> Title </label>{' '}
           <input
             type='text'
-            onChange={event => inputChangeHandler('title', event)}
+            value={enteredTitle}
+            onChange={titleChangeHandler}
           />{' '}
         </div>{' '}
         <div className='new-expense__control'>
@@ -64,7 +67,8 @@ const ExpenseForm = () => {
             type='number'
             min='0.01'
             step={0.01}
-            onChange={event => inputChangeHandler('amount', event)}
+            value={enteredAmount}
+            onChange={amountChangeHandler}
           />{' '}
         </div>{' '}
         <div className='new-expense__control'>
@@ -73,7 +77,8 @@ const ExpenseForm = () => {
             type='date'
             min='2019-01-01'
             max='2022-12-31'
-            onChange={event => inputChangeHandler('date', event)}
+            value={enteredDate}
+            onChange={dateChangeHandler}
           />{' '}
         </div>{' '}
       </div>{' '}
