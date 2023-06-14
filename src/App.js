e6ad3a7c088f2bd2828dd043bfdd7a7fb0a4ac69
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2023-06-14 21:11:26 +0300

    add expense form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import NewExpense from './components/NewExpense/NewExpense'
 import Expenses from './components/Expenses/Expenses'
 
 const App = () => {
@@ -34,7 +35,8 @@ const App = () => {
 
   return (
     <div>
-      <h2> Let 's get started!</h2> <Expenses items={expenses} />{' '}
+      <NewExpense />
+      <Expenses items={expenses} />{' '}
     </div>
   )
 }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,25 +5,22 @@ import Card from '../UI/Card'
 import './ExpenseItem.css'
 
 const ExpenseItem = props => {
-    const [title, setTitle] = useState(props.title)
+  const [title, setTitle] = useState(props.title)
 
-    // let title = props.title
-    const clickHandler = () => {
-        setTitle('Updated!')
-    }
-    return ( <
-        Card className = 'expense-item' >
-        <
-        ExpenseDate date = { props.date }
-        />{' '} <
-        div className = 'expense-item__description' >
-        <
-        h2 > { title } < /h2>{' '} <
-        div className = 'expense-item__price' > $ { props.amount } < /div>{' '} <
-        /div>{' '} <
-        button onClick = { clickHandler } > Change Title < /button>{' '} <
-        /Card>
-    )
+  // let title = props.title
+  const clickHandler = () => {
+    setTitle('Updated!')
+  }
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />{' '}
+      <div className='expense-item__description'>
+        <h2> {title} </h2>{' '}
+        <div className='expense-item__price'> $ {props.amount} </div>{' '}
+      </div>{' '}
+      <button onClick={clickHandler}> Change Title </button>{' '}
+    </Card>
+  )
 }
 
 export default ExpenseItem
