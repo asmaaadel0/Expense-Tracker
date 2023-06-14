4f2f7240c28b436cfcea09297157cb2e7114c99b
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2023-06-14 21:15:17 +0300

    update styling

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -8,12 +8,10 @@ const ExpenseForm = () => {
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label> Title </label> <input type='text' />
-        </div>
-        ;{' '}
+        </div>{' '}
         <div className='new-expense__control'>
           <label> Amount </label> <input type='number' min='0.01' step={0.01} />{' '}
-        </div>
-        ;{' '}
+        </div>{' '}
         <div className='new-expense__control'>
           <label> Date </label>{' '}
           <input type='date' min='2019-01-01' max='2022-12-31' />
