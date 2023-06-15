d09cbdeac50361a7a8f3f19fdfd38a9be284c078
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2023-06-15 21:53:40 +0300

    update filter

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,13 +5,15 @@ import Card from '../UI/Card'
 import './ExpenseItem.css'
 
 const ExpenseItem = props => {
-    const [title, setTitle] = useState(props.title)
+    const [title] = useState(props.title)
 
     // let title = props.title
     // const clickHandler = () => {
     //   setTitle('Updated!')
     // }
     return ( <
+        li >
+        <
         Card className = 'expense-item' >
         <
         ExpenseDate date = { props.date }
@@ -20,8 +22,9 @@ const ExpenseItem = props => {
         <
         h2 > { title } < /h2>{' '} <
         div className = 'expense-item__price' > $ { props.amount } < /div>{' '} <
-        /div>{' '} { /* <button onClick={clickHandler}> Change Title </button>{' '}{' '} */ } <
-        /Card>
+        /div>{' '} { /* <button onClick={clickHandler}> Change Title </button>{' '}{' '} */ } { ' ' } <
+        /Card>{' '} <
+        /li>
     )
 }
 
