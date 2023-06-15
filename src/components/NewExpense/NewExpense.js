1ae63ef60a1e12fb1d414502e2e27739f9e2f8dc
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2023-06-15 22:10:22 +0300

    update show form condition

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -4,18 +4,20 @@ import ExpenseForm from './ExpenseForm.js'
 
 import './NewExpense.css'
 const NewExpense = props => {
-  const saveExpenseData = enteredExpenseData => {
-    const expenseData = {
-      ...enteredExpenseData,
-      id: Math.random().toString()
+    const saveExpenseData = enteredExpenseData => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        }
+        props.onAddNewExpense(expenseData)
     }
-    props.onAddNewExpense(expenseData)
-  }
 
-  return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseData} />{' '}
-    </div>
-  )
+    return ( <
+        div className = 'new-expense' >
+        <
+        ExpenseForm onSaveExpenseData = { saveExpenseData }
+        />{' '} <
+        /div>
+    )
 }
 export default NewExpense
