03ba798a3fc90d0a3f6a53232d9caacd00607a16
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2023-06-15 19:16:18 +0300

    fix bug

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -19,7 +19,8 @@ const Expenses = props => {
         onFilterYear = { filterYear }
         /> {''}{' '} {
             props.items.map(expense => ( <
-                ExpenseItem title = { expense.title }
+                ExpenseItem key = { expense.id }
+                title = { expense.title }
                 amount = { expense.amount }
                 date = { expense.date }
                 />
