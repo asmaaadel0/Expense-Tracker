9a6e26efbcdda2e8cdec8b4355a6e89e00118ea9
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2023-06-16 00:18:16 +0300

    update chart

--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Chart from "../Chart/Chart";
-const ExpenseChart = (props) => {
+
+const ExpensesChart = (props) => {
   const chartDataPoints = [
     { label: "Jan", value: 0 },
     { label: "Feb", value: 0 },
     { label: "Mar", value: 0 },
     { label: "Apr", value: 0 },
+    { label: "May", value: 0 },
     { label: "Jun", value: 0 },
     { label: "Jul", value: 0 },
     { label: "Aug", value: 0 },
@@ -17,11 +19,11 @@ const ExpenseChart = (props) => {
   ];
 
   for (const expense of props.expenses) {
-    const date = new Date(expense.date);
-    const expenseMonth = date.getMonth();
+    const expenseMonth = expense.date.getMonth(); // starting at 0 => January => 0
     chartDataPoints[expenseMonth].value += expense.amount;
   }
+
   return <Chart dataPoints={chartDataPoints} />;
 };
 
-export default ExpenseChart;
+export default ExpensesChart;
